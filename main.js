ee9a99b1c695a902219c0604a0162865db7b53fc
Franciele Torres <mariafrancielesm@gmail.com>
2022-06-13 01:53:26 +0100

    Aula 5

--- main.js
+++ main.js
@@ -4,6 +4,54 @@ onScroll()
 function onScroll() {
   showNavOnScroll()
   showBackToTopButtonOnScroll()
+
+  activateMenuAtCurrentSection(home)
+  activateMenuAtCurrentSection(services)
+  activateMenuAtCurrentSection(about)
+  activateMenuAtCurrentSection(contact)
+}
+
+function activateMenuAtCurrentSection(section) {
+  // linha alvo
+  const targetLine = scrollY + innerHeight / 2
+
+  //verificar se a secao passou da linda
+  //quais dados vou precisar?
+
+  // o topo da seçao
+  const sectionTop = section.offsetTop
+
+  // a altura da seçao
+  const sectionHeight = section.offsetHeight
+
+  // o topo da seçao chegou ou ultrapassou a linha alvo
+  const sectionTopReachOrPassedTargetline = (targetLine >= sectionTop)
+
+  // informacoes dos dados e da lógica
+  console.log(
+    'O topo da seção chegou ou passou da linha?',
+    sectionTopReachOrPassedTargetline
+  )
+
+  // a seçao termina onde ?
+  const sectionEndsAt = (sectionTop + sectionHeight)
+
+  //o final da seçao passou da linha alvo
+  const sectionEndPassedTargetline = (sectionEndsAt <= targetLine)
+
+  console.log('O fundo da seçao passou da linha?', sectionEndPassedTargetline)
+
+  // limites da seçao
+  const sectionBoundaries =
+    sectionTopReachOrPassedTargetline && !sectionEndPassedTargetline
+
+  const sectionId = section.getAttribute('id')
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+  menuElement.classList.remove('active')
+  if (sectionBoundaries) {
+    menuElement.classList.add('active')
+  }
 }
 
 function showNavOnScroll() {
